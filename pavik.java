1b4bbc9a5fc3d55f2a244ead86a2270dc2bd422f
pavithrak03 <117439478+pavithrak03@users.noreply.github.com>
2024-12-08 15:09:44 +0530

    Update pavik.java

--- pavik.java
+++ pavik.java
@@ -3,5 +3,9 @@ public class Demo
     public static void main(String [] args)
     {
       System.out.println("hello");
+      System.out.println("pavithrak");
+      System.out.println("mysore");
+      System.out.println("bangalore");
+      
     }
   }
